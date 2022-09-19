3f05cfa64978a1891489074cf4245672e3ef92a9
2808 <charusat.edu.in>
2022-09-19 14:13:30 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,13 @@
+const add=(a,b)=>{
+    return a+b;
+}
+const sub=(a,b)=>{
+    return a-b;
+}
+const multi=(a,b)=>{
+    return a*b;
+}
+const div=(a,b)=>{
+    a/b;
+}
+module.export={add,sub,multi,div}
---
+++ input.js
@@ -0,0 +1,6 @@
+const{add,sub,multi,div} =require("./calculator.js")
+
+console.log(add(5,6));
+console.log(sub(8,3));
+console.log(multi(4,7));
+console.log(div(8,2));
